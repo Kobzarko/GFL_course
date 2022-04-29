af8ab1dfe75ad42e1e6c72181efab1f5412daac9
Kobzarko <pyshievgeeks@gmail.com>
2022-04-29 23:20:58 +0300

    Sign In pages: home, auth, createAccount, myAccount

--- steps.d.ts
+++ steps.d.ts
@@ -1,8 +1,10 @@
 /// <reference types='codeceptjs' />
 type steps_file = typeof import('./steps_file.js');
+type homePage = typeof import('./pages/home.js');
+type authPage = typeof import('./pages/auth.js');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any }
+  interface SupportObject { I: I, current: any, homePage: homePage, authPage: authPage }
   interface Methods extends Playwright {}
   interface I extends ReturnType<steps_file> {}
   namespace Translation {
--- store_test.js
+++ store_test.js
@@ -1,6 +1,20 @@
+let user = {
+    firstName: 'Test',
+    lastName: 'Automation',
+    state: 'Alabama',
+    city: 'Birmingham',
+    address: '801 Tom Martin Dr.',
+    postalCode: '35211',
+}
+
 Feature('Store');
 
-Scenario('test something', ({ I }) => {
-    I.amOnPage('http://automationpractice.com/index.php');
-    pause();
+Scenario('Sign In', ({ I, homePage, authPage,createAccountPage,myAccountPage }) => {
+    homePage.openStore();
+    homePage.clickSignIn();
+    authPage.fillNewUserEmail(Date.now() + '@test.com');
+    authPage.clickCreateAccount();
+    createAccountPage.fillNewUserForm(user);
+    myAccountPage.verifyMyAccount();
+    
 });
