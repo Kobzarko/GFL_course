56c4fdbf935f96b48edb0e0c72094d047c13f253
Kobzarko <pyshievgeeks@gmail.com>
2022-05-04 10:53:16 +0300

    fillField(), waitForPageLoad()

--- pages/auth.js
+++ pages/auth.js
@@ -1,19 +1,29 @@
 const { I } = inject();
 
 module.exports = {
+
   newUserEmailInput: {css: '#email_create'},
   createAccountButton: {css: '#SubmitCreate'},
+  emailInput: {css: 'input[name=email_create]'},
+  currentUserEmailInput: {css: '#email'},
+  currentUserPassInput: {css: '#passwd'},
 
-  waitForPageLoad() {
-    I.waitForVisible(this.newUserEmailInput);
+  waitForPageLoad(field) {
+    I.waitForVisible(field);
   },
   
-  fillNewUserEmail(email) {
-    this.waitForPageLoad();
-    I.fillField(this.newUserEmailInput, email);
+  fillField(value,field) {
+    this.waitForPageLoad(field);
+    I.fillField(field, value);
   },
 
   clickCreateAccount() {
     I.click(this.createAccountButton);
-  }
+  },
+
+  async getUserEmail(user){
+    let email = await I.grabValueFrom(this.emailInput);
+    return user.email = email;
+  },
+
 }
