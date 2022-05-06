236f38ae5bf97204eb580ae41c99ce32d19eb927
Kobzarko <pyshievgeeks@gmail.com>
2022-05-06 12:27:51 +0300

    clickTshirt,clickImage, don't run moveCursorTo

--- pages/myAccount.js
+++ pages/myAccount.js
@@ -2,9 +2,27 @@ const { I } = inject();
 
 module.exports = {
 
+  // tshirt: locate('a').withText('T-shirts'),
+  // css=E[A*='t']
+  tshirt: {css: 'ul.sf-menu.clearfix.menu-content.sf-js-enabled.sf-arrows>:last-child'},
+  imgTshirt: {css: 'a.product_img_link>img.replace-2x.img-responsive'},
+  // tshirt: locate('ul.sf-menu clearfix menu-content sf-js-enabled sf-arrows').last(),
+
   verifyMyAccount() {
-    // I.see('MY ACCOUNT');
     I.seeTitleEquals('My account - My Store');
   },
+
+  clickTshirt(){
+    I.waitForVisible(this.tshirt);
+    I.click(this.tshirt);
+    console.log(this.imgtshirt);
+  },
+ 
+  clickImage(){
+    // I.waitForVisible(this.imgtshirt);
+    I.scrollTo(this.imgTshirt);
+    I.moveCursorTo(this.imgtshirt);
+    // I.click('a.product_img_link>img.replace-2x.img-responsive');
+  }
 
 }
