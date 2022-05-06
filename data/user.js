65770fe0bae44f10452a631b12cdcbf97834c745
Kobzarko <pyshievgeeks@gmail.com>
2022-05-06 12:32:18 +0300

    email added for sign in

--- data/user.js
+++ data/user.js
@@ -5,6 +5,6 @@ module.exports = {
     city: 'Birmingham',
     address: '801 Tom Martin Dr.',
     postalCode: '35211',
-    email: '',
+    email: '1651819778065@test.com', // added value for sign in
     password: 'Temp123456!',
 }
